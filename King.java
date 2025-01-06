40980f2aa8ee3ea494334ed3f4b472609f41864d
AndrewBond-005 <121750375+AndrewBond-005@users.noreply.github.com>
2025-01-07 00:39:35 +0300

    Update King.java

--- King.java
+++ King.java
@@ -1,27 +1,35 @@
 import java.util.ArrayList;
+
 public class King {
-    private Object ObjectInHand;
+    private Object objectInHand;
     private ArrayList<Decree> decree = new ArrayList<>();
+
     void take(Ring ring) {
-        this.ObjectInHand=ring;
-        System.out.print("Король взял "+ring.name()+" ");
+        this.objectInHand = ring;
+        System.out.print("Король взял " + ring.name() + " ");
     }
-    public Object getObjectInHand() {return ObjectInHand;}
+
+    public Object getObjectInHand() {
+        return objectInHand;
+    }
+
     public void setDecree(ArrayList<Decree> decree) {
         this.decree = decree;
     }
+
     public ArrayList<Decree> getDecree() {
         return decree;
     }
+
     ArrayList<Notifier> send(int count, ArrayList<String> arrayList) throws CloneNotSupportedException {
         ArrayList<Notifier> list = new ArrayList<>();
-        System.out.print("разослал "+count+" ");
-        for(String name : arrayList){
+        System.out.print("разослал " + count + " ");
+        for (String name : arrayList) {
             Notifier notifier = new Notifier(name);
-            for( int i=1;i<=count;i++){
+            for (int i = 1; i <= count; i++) {
                 list.add(notifier.clone());
-                if(i==1){
-                    System.out.print(name+", ");
+                if (i == 1) {
+                    System.out.print(name + ", ");
                 }
             }
         }
