24d9cef3cb445a775292cc6fc3069572a511285f
AndrewBond-005 <121750375+AndrewBond-005@users.noreply.github.com>
2025-01-07 00:40:59 +0300

    Update Prince.java

--- Prince.java
+++ Prince.java
@@ -1,43 +1,66 @@
 import java.util.Objects;
 
-public class Prince extends Spouse {
-    private Woman wife=null;
+public class Prince extends Human {
+    private Woman wife = null;
     private String name;
+
     public Prince(String name) {
         this.name = name;
     }
+
     @Override
-    void marry(Spouse wife) { this.wife= (Woman) wife;}
+    public void marry(Human wife) {
+        this.wife = (Woman) wife;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
-    public void setWife(Spouse wife) {
-        this.wife = (Woman)wife;
-    }
-    public Spouse getWife() {
+
+    public Human getWife() {
         return wife;
     }
+
+    public void setWife(Woman wife) {
+        this.wife = wife;
+    }
+
     public String getName() {
         return name;
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-        Prince prince = (Prince) o;
-        return Objects.equals(wife, prince.wife) && Objects.equals(name, prince.name);
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (obj.getClass() != this.getClass()) {
+            return false;
+        }
+        final Prince other = (Prince) obj;
+        if ((this.name == null) ? (other.name != null) : !this.name.equals(other.name)) {
+            return false;
+        }
+        if ((this.wife == null) ? (other.wife != null) : !this.wife.equals(other.wife)) {
+            return false;
+        }
+        return true;
     }
+
     @Override
+    public int hashCode() {
+        int hash = 19;
+        hash = 89 * hash + (this.name != null ? this.name.hashCode() : 0);
+        hash = 89 * hash + (this.wife != null ? this.wife.hashCode() : 0);
+        return hash;
+    }
+
     public String toString() {
         return "Prince{" +
                 "wife=" + wife +
-                ", name='" + name  +
+                ", name='" + name +
                 '}';
     }
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), wife, name);
-    }
+
+
 }
